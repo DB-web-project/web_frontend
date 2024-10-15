1abfe99bdd0e87b3889833d6c50d9ffa74e7db41
dongfengwuyishi <149307614+dongfengwuyishi@users.noreply.github.com>
2024-10-15 16:20:18 +0800

    小改动

--- src/config/default/setting.config.js
+++ src/config/default/setting.config.js
@@ -17,10 +17,10 @@ module.exports = {
   multiPage: false,                     //多页签模式，true:开启，false:不开启
   cachePage: true,                      //是否缓存页面数据，仅多页签模式下生效，true 缓存, false 不缓存
   hideSetting: false,                   //隐藏设置抽屉，true:隐藏，false:不隐藏
-  systemName: 'AA Comments',         //系统名称
+  systemName: '航评万象',         //系统名称
   copyright: '2018 ICZER 工作室出品',     //copyright
   asyncRoutes: false,                   //异步加载路由，true:开启，false:不开启
-  showPageTitle: true,                  //是否显示页面标题（PageLayout 布局中的页面标题），true:显示，false:不显示
+  showPageTitle: false,                  //是否显示页面标题（PageLayout 布局中的页面标题），true:显示，false:不显示
   filterMenu: true,                    //根据权限过滤菜单，true:过滤，false:不过滤
   animate: {                            //动画设置
     disabled: false,                    //禁用动画，true:禁用，false:启用
